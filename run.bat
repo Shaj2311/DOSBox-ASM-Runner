3ea28da6c4188902f31042925ead8c27c2918257
Haider Ali <haideralijafri2007@gmail.com>
2025-08-30 13:29:29 +0500

    Add batch file

---
+++ run.bat
@@ -0,0 +1,38 @@
+@echo off
+
+REM Terminate if no argument passed
+if "%~1"=="" (
+    echo Error: No argument passed
+    exit /b
+)
+
+REM Extract file name without extension
+set "fileTitle=%~n1"
+
+REM Create temporary config (fullscreen, auto mount, auto compile and debug)
+> temp.conf echo [sdl]
+>> temp.conf echo fullscreen = true
+>> temp.conf echo [dosbox]
+>> temp.conf echo [render]
+>> temp.conf echo [cpu]
+>> temp.conf echo [mixer]
+>> temp.conf echo [midi]
+>> temp.conf echo [sblaster]
+>> temp.conf echo [gus]
+>> temp.conf echo [speaker]
+>> temp.conf echo [joystick]
+>> temp.conf echo [serial]
+>> temp.conf echo [dos]
+>> temp.conf echo [ipx]
+>> temp.conf echo [autoexec]
+>> temp.conf echo mount c "C://Users/Haider Ali/source/COAL/tools"
+>> temp.conf echo c:
+>> temp.conf echo nasm %fileTitle%.asm -o %fileTitle%.com
+>> temp.conf echo afd %fileTitle%.com
+
+
+REM Launch dosbox using temp.conf
+"C://Users/Haider Ali/Source/COAL/DOSBoxPortable/App/DOSBox/DOSBox.exe" -conf temp.conf
+
+REM delete temp.conf once DOSBox is launched
+del temp.conf
