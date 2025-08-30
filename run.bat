1f0efcaeb7b8ee59f23a9c30f46ec8d1c26edad4
Shaj2311 <haideralijafri2007@gmail.com>
2025-08-30 13:55:02 +0500

    Edit run.bat to handle file not found

--- run.bat
+++ run.bat
@@ -6,6 +6,12 @@ if "%~1"=="" (
     exit /b
 )
 
+REM Terminate if file does not exist
+if not exist "%~f1" (
+    echo Error: File "%~1" not found.
+    exit /b
+)
+
 REM Extract file name without extension
 set "fileTitle=%~n1"
 
@@ -25,15 +31,14 @@ REM Create temporary config (fullscreen, auto mount, auto compile and debug)
 >> temp.conf echo [dos]
 >> temp.conf echo [ipx]
 >> temp.conf echo [autoexec]
->> temp.conf echo mount c C://Path/To/Your/ASM/Files
+>> temp.conf echo mount c "C://Users/Haider Ali/source/COAL/tools"
 >> temp.conf echo c:
 >> temp.conf echo nasm %fileTitle%.asm -o %fileTitle%.com
 >> temp.conf echo afd %fileTitle%.com
 
 
 REM Launch dosbox using temp.conf
-C://Path/To/DOSBox.exe -conf temp.conf
+"C://Users/Haider Ali/Source/COAL/DOSBoxPortable/App/DOSBox/DOSBox.exe" -conf temp.conf
 
 REM delete temp.conf once DOSBox is launched
 del temp.conf
-
